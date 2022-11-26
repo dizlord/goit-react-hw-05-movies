175949e2bea92dbac2fd69f6254adb16a49548b6
Illia Kazmin <kazmin1c@gmail.com>
2022-11-26 22:57:22 +0200

    add Routes to App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,23 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "pages/Home/Home";
+import { Movies } from "pages/Movies/Movies";
+import { MovieDetails } from "pages/MovieDetails/MovieDetails";
+import { Navigation } from "./Navigation/Navigation";
+import { Cast } from "./Cast/Cast";
+import { Reviews } from "./Reviews/Reviews";
+
 export const App = () => {
   return (
     <div>
-      React homework template
+      <Navigation/>
+      <Routes>
+        <Route path='/' element={<Home />} index />
+        <Route path='movies' element={<Movies />} />
+        <Route path='movies/:movieId' element={<MovieDetails />}>
+          <Route path='cast' element={<Cast />} />
+          <Route path='reviews' element={<Reviews/>}/>
+        </Route>
+      </Routes>
     </div>
   );
 };
